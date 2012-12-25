e913621df0dcc06603f442a5d1a4b7b5f5f3b722
dlluncor <david.lluncor@gmail.com>
2012-12-24 22:41:35 -0800

    Determining the number of times it takes to answer the solution correctly.

--- mm2.py
+++ mm2.py
@@ -26,20 +26,6 @@ def _Translate(my_guess):
     trans.append(d[char])
   return ' '.join(trans) 
 
-def _ColorsNotMine(mycolor):
-  """Returns a list of colors that are not mine. A color is a single character '1'."""
-  colors = [str(num) for num in xrange(NUM_CHARS)]
-  colors.remove(mycolor)
-  return colors
-
-def _GetUnguessed(arr_of_guessed):
-  """Given [True, True, False] returns [2], the unguessed indices."""
-  unguessed = []
-  for index in xrange(len(arr_of_guessed)):
-    if not arr_of_guessed[index]:
-      unguessed.append(index)
-  return unguessed
-
 def _InvalidSolution(num_blacks, num_whites, guess, potential_sol):
   """If there are more than num_blacks exact overlaps, it is an invalid solution."""
   matches = 0
@@ -59,117 +45,12 @@ def _InvalidSolution(num_blacks, num_whites, guess, potential_sol):
     return True 
   return False
  
-
 def _PossAns2(guess, num_blacks, num_whites, possibles):
   new_possibles = []
   for possible in possibles:
     if not _InvalidSolution(num_blacks, num_whites, guess, possible):
       new_possibles.append(possible)
   return new_possibles
-
-def _PossAns(guess, num_blacks, num_whites):
-  """Returns a set of tuples with possible answers.
-
-  """
-  """
-    Reject invalid solutions when there are not all blacks, then there cannot be
-    that same color in that same position.
-  """
-  answers = set([])  # Fill with potential answers. TODO(dlluncor): why so many dups?
-  def _FindAns(blacks, whites, blanks, unfilled_indices, used_for_guess,
-               potential_sol):
-    """
-    Args:
-      used_for_guess: Which part of the secret I've used to construct my guess.
-    """
-    if blacks > 0:
-      # Black rule.
-      blacks -= 1
-      # Any unused index can be set to the color in the guess.
-      unguessed_indices = _GetUnguessed(used_for_guess)
-      for index in unguessed_indices:
-        unfilled = unfilled_indices[:]
-        unfilled.remove(index)
-
-        used_guess = used_for_guess[:]
-        used_guess[index] = True
-
-        color_from_guess = guess[index]
-        pot_sol = potential_sol[:]
-        pot_sol[index] = color_from_guess # Set that color to be a valid one.
-        if _InvalidSolution(num_blacks, num_whites, guess, pot_sol):
-          continue
-        _FindAns(blacks, whites, blanks, unfilled, used_guess, pot_sol)
-    elif whites > 0:
-      whites -= 1
-      # Apply the white rule to all unguessed indices.
-      unguessed_indices = _GetUnguessed(used_for_guess)
-      for pos_to_guess in unguessed_indices:
-        # For any unused index, set the item to that color that is not my index though.
-        indices_to_use = unfilled_indices[:]
-        if DEBUG:
-          #print 'Pos to guess: %d' % pos_to_guess
-          #print 'Unfilled inds: %s' % str(indices_to_use)
-          pass
-        if pos_to_guess in indices_to_use: # TODO: Shouldn't this be true though? Huh?
-          indices_to_use.remove(pos_to_guess)
-        else:
-          if DEBUG:
-            print 'wtf a problem white'
-        # Fill in indices for which we know the color cannot be there.
-        for index in indices_to_use:
-          unfilled = unfilled_indices[:]
-          unfilled.remove(index)
-
-          used_guess = used_for_guess[:]
-          used_guess[pos_to_guess] = True # Hm?
-
-          color_from_guess = guess[pos_to_guess]
-          pot_sol = potential_sol[:]
-          pot_sol[index] = color_from_guess
-          # Scrub invalid solutions any time a potential solution now has more
-          # than black overlaps.
-          if _InvalidSolution(num_blacks, num_whites, guess, pot_sol):
-            continue
-          _FindAns(blacks, whites, blanks, unfilled, used_guess, pot_sol)
-
-    elif blanks > 0:
-      blanks -= 1
-      # Go through the unused indices.
-      unguessed_indices = _GetUnguessed(used_for_guess)
-      for pos_to_guess in unguessed_indices:
-        # Apply the blank rule to all unset indices just in case.
-        indices_to_use = unfilled_indices[:]
-        for index in indices_to_use: 
-          color_from_guess = guess[pos_to_guess]
-          # Find all other colors that are not this guess.
-          diff_colors = _ColorsNotMine(color_from_guess)
-          for diff_color in diff_colors:
-            unfilled = unfilled_indices[:]
-            if index in unfilled: # TODO: shouldnt this always be true though?
-              unfilled.remove(index)
-            else:
-              print 'wtf a problemm blank' 
-            used_guess = used_for_guess[:]
-            used_guess[pos_to_guess] = True
-
-            pot_sol = potential_sol[:]
-            pot_sol[index] = diff_color
-            if _InvalidSolution(num_blacks, num_whites, guess, pot_sol):
-              continue
-            _FindAns(blacks, whites, blanks, unfilled, used_guess, pot_sol)
-    else:
-      # We have reached the end horray.
-      answers.add(tuple(potential_sol))
-
-  num_blanks = NUM_SPACES - num_whites - num_blacks
-  unfilled_indices = [ind for ind in xrange(NUM_SPACES)] #[0, 1, 2, 3, 4]
-  used_for_guess = [False for _ in xrange(NUM_SPACES)] #[False, False, False, False, False]
-  potential_sol = [-1 for _ in xrange(NUM_SPACES)] #[-1, -1, -1, -1, -1]
-  _FindAns(num_blacks, num_whites, num_blanks,
-           unfilled_indices, used_for_guess,
-           potential_sol)
-  return answers
 
 class Guesser(object):
   
@@ -201,13 +82,10 @@ class Guesser(object):
       print 'Current answer: %s' % str(guess)
       print 'Num whites: %d' % whites
       print 'Num blacks: %d' % blacks
-    now_possibles = set(_PossAns2(guess, blacks, whites, self.possibles))
-    #now_possibles = _PossAns(guess, blacks, whites)
+    self.possibles = _PossAns2(guess, blacks, whites, self.possibles)
     if DEBUG:
-      #print 'Prev solutions are: %s' % str(self.possibles)
       print 'Possible solutions are: %s' % str(now_possibles)
     # Find the union of previous possible answers and my possible answers.
-    self.possibles = list(now_possibles.intersection(set(self.possibles)))
     if DEBUG:
       #print 'Union of possibles is: %s' % str(self.possibles)
       pass
@@ -252,15 +130,13 @@ def _GetFeedback(current_ans, correct_ans):
     print 'Enter %d pegs with a space (3b 2w):' % NUM_SPACES
     return raw_input()
 
-def main(argv):
-  if len(argv) < 2:
-    print 'Print your %d length secret with characters 0 to %d' % (NUM_SPACES,
-        NUM_CHARS - 1)
-    return
-  if len(argv[1]) != NUM_SPACES:
-    print 'Your secret must be %d characters long' % NUM_SPACES
-    return
-  ans = argv[1] # '1211'
+def StartGuessing(ans):
+  """
+  Args:
+    ans: the answer as a string, e.g., 12121
+  Returns:
+    The number of guesses it takes to solve this problem.
+  """
   ans_tup = tuple([char for char in ans])
   for char in ans_tup:
     if char not in VALID_CHARS:
@@ -282,8 +158,30 @@ def main(argv):
       (blacks, whites) = myfeedback.split(' ')
       print 'Blacks: %d, Whites: %d' % (int(blacks), int(whites))
       guesser.Prune(curans, int(whites), int(blacks))
-  print 'Num guesses: %d' % guesser.num_guesses 
-  
+  print 'Num guesses: %d' % guesser.num_guesses
+  return guesser.num_guesses
+
+def Simulate(ans):
+  N = 100
+  guess_list = [StartGuessing(ans) for _ in xrange(N)]
+  avg = sum(guess_list) / N
+  print '-' * 20
+  print 'Guesses amounts: %s' % str(guess_list)
+  print 'Average number of guesses: %f' % avg
+  x2_list = [guess_num * guess_num for guess_num in guess_list] 
+  stdev = math.sqrt(sum(x2_list)/N - math.pow(sum(guess_list)/N, 2))
+  print 'Standard deviation: %f' % stdev
+ 
+def main(argv):
+  if len(argv) < 2:
+    print 'Print your %d length secret with characters 0 to %d' % (NUM_SPACES,
+        NUM_CHARS - 1)
+    return
+  if len(argv[1]) != NUM_SPACES:
+    print 'Your secret must be %d characters long' % NUM_SPACES
+    return
+  ans = argv[1] # '1211'
+  Simulate(ans)
 
 if __name__ == '__main__':
   main(sys.argv)
